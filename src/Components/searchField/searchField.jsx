e9140b3415e114a49e376b0ace748e522e0439c7
Vimal Raveendran <vimalraveendra@gmail.com>
2021-03-05 20:25:46 +0100

    updated index styles sheet,added google fonts, also updated App component & searchField components to render the input text vased onchange events

--- src/Components/searchField/searchField.jsx
+++ src/Components/searchField/searchField.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import "./searchField.css";
 import {FaSistrix } from 'react-icons/fa';
 
-const SearchField = ()=>{
+const SearchField = ({searchChange})=>{
     return(
         <div className="searchField-container">
         <FaSistrix className="search-icon"/>
          <input
          type="search"
+         onChange={searchChange}
          />
         </div>
     )
